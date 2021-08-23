02d80ebc5d4dc39de291abe6be47bc9c512b1f5f
omegamaximus10 <bryan_2904@hotmail.com>
2021-08-23 19:23:03 +0000

    ALLDONE

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,json
 app = Flask(__name__)
 
 
@@ -14,12 +14,17 @@ def hello_world():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.data
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+     request_body = request.data
+     decoded_object = json.loads(request_body)
+     todos.append(decoded_object)
+     print("Incoming request with the following body", request_body)
+     return jsonify(todos)
 
-
-
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
 
 if (__name__) == "__main__":
     app.run (host= '0.0.0.0',port= 3245, debug= True)
